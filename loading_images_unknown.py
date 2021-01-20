9789cddeb4dfcb11bbca03187af9d851012e26fc
deep890 <64359511+deep890@users.noreply.github.com>
2021-01-20 09:15:01 +0530

    Create loading_images_unknown.py

---
+++ loading_images_unknown.py
@@ -0,0 +1,5 @@
+!mkdir unknown
+!wget https://i.insider.com/5ddfa893fd9db26b8a4a2df7 -O unknown/1.jpg
+!wget https://cdn-images-1.medium.com/max/1200/1*aEoYLgy4z1lT1kW7dqWzBg.jpeg -O unknown/2.jpg
+!wget https://media2.s-nbcnews.com/j/newscms/2017_46/2224911/171113-bill-gates-se-247p_043bd413c63b2a97abf11bfad747538d.fit-760w.jpg -O unknown/3.jpg
+!wget https://specials-images.forbesimg.com/imageserve/1184274010/960x0.jpg -O unknown/4.jpg
