8e217d9a5808b918eaaa60ccc47d73ab5f8a13a9
deep890 <64359511+deep890@users.noreply.github.com>
2021-01-20 09:14:15 +0530

    Create loadin_images_known.py

---
+++ loadin_images_known.py
@@ -0,0 +1,5 @@
+!mkdir known
+!wget https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg -O known/elon.jpg
+!wget https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X_400x400.jpg -O known/bill.jpg
+!wget https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg -O known/obama.jpg
+
